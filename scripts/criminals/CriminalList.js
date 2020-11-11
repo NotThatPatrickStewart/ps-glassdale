ff7173395519382f5d5df4e12eef952fca63d62e
NotThatPatrickStewart <patrickstewart1723@gmail.com>
2020-11-11 13:50:50 -0600

    refactored CriminalList and render functions to iterate through criminals and facilities and display the relationship between them on the dom

--- scripts/criminals/CriminalList.js
+++ scripts/criminals/CriminalList.js
@@ -2,13 +2,15 @@ import { getCriminals, useCriminals } from "./CriminalProvider.js";
 import { Criminal } from './Criminal.js'
 import { useConvictions } from "../convictions/ConvictionProvider.js";
 import { useOfficers } from "../officers/OffficerProvider.js";
+import { getFacilities, useFacilities } from "../facility/FacilityProvider.js";
+import { getCriminalFacilities, useCriminalFacilities } from "../facility/CriminalFacilityProvider.js";
 
 const criminalsContainer = document.querySelector(".criminalsContainer")
 const eventHub = document.querySelector(".container")
 
 eventHub.addEventListener("CrimeChosen", event => {
     if (event.detail.crimeThatWasChosen !== "0") {
-        console.log("event:", event)
+        // console.log("event:", event)
 
         const crimeId = event.detail.crimeThatWasChosen
         const foundCrime = useConvictions().find(conviction => {
@@ -24,7 +26,7 @@ eventHub.addEventListener("CrimeChosen", event => {
 eventHub.addEventListener("officerChosen", event => {
     if (event.detail.officer !== "0") {
         // console.log("CriminalList: officerSelected custom event has been heard on the event hub, selected officer name: ", selectedOfficerName)
-        console.log ("event", event)
+        console.log("event", event)
 
         const officerId = event.detail.officer
         const foundOfficer = useOfficers().find(arrestingOfficer => {
@@ -38,25 +40,43 @@ eventHub.addEventListener("officerChosen", event => {
 })
 
 
-const render = criminalCollection => {
-    let criminalsHTMLRepresentation = ""
-    for (const criminal of criminalCollection)
+const render = (criminalCollection, facilityCollection, relationshipCollection) => {
+    // let criminalsHTMLRepresentation = ""
+    // for (const criminal of criminalCollection)
 
-        criminalsHTMLRepresentation += Criminal(criminal)
+    //     criminalsHTMLRepresentation += Criminal(criminal)
+    //iterate all criminals
+    criminalsContainer.innerHTML = ` <h2>Criminals of Glassdale</h2>
+    <section class="criminalsList">
+    ${criminalCollection.map(
+        (criminalObj) => {
+            //filter all relationships to get only those for this criminal
+            const facilitiesForGivenCriminal = relationshipCollection.filter(crimFac => crimFac.criminalId === criminalObj.id)
 
-    criminalsContainer.innerHTML = `
-        <h2>Criminals of Glassdale</h2>
-        <section class="criminalsList">
-            ${criminalsHTMLRepresentation}
-        </section>
-    `
+            //convert the relationships to facilities with .map()
+            const facilities = facilitiesForGivenCriminal.map(crimFac => {
+                const matchingFacilityObj = facilityCollection.find(facility => facility.id === crimFac.facilityId)
+                return matchingFacilityObj
+            })
+                // console.log("facilities", facilities)
+            //pass the matching facilities to Criminal.js
+            return Criminal(criminalObj, facilities)
+        }
+    ).join("") } </section>`
 }
 
 
 
 export const CriminalList = () => {
-    getCriminals().then(() => {
-        const appStateCriminals = useCriminals()
-        render(appStateCriminals)
-    })
+    getCriminals()
+        .then(getFacilities)
+        .then(getCriminalFacilities)
+        .then(
+            () => {
+                const criminalsArray = useCriminals()
+                const facilitiesArray = useFacilities()
+                const criminalFacilitiesArray = useCriminalFacilities()
+                render(criminalsArray, facilitiesArray, criminalFacilitiesArray)
+                // console.log("criminals array:", criminalsArray, "facilities array:", facilitiesArray, "criminal facilities array:", criminalFacilitiesArray)
+            })
 }
